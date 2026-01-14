509d32a5536ff09ad9f04737b54012e5263fab44
Roberth <roberthchirinos87@gmail.com>
2026-01-14 02:43:12 +0100

    Create environment files to extract env variables for the FE

---
+++ apps/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// This file is used for production builds
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000/api', // production API URL
+};
---
+++ apps/frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// This file is used for development
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+};
